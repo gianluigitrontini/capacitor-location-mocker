5aff0295b88b03941d40d3476d701af777387648
Gianluigi Trontini <g.trontini@nbsgroup.it>
2023-05-13 10:55:07 +0200

    Aggiunta plugin geolocation come riferimento

---
+++ android/app/src/main/java/io/ionic/starter/EchoPlugin.java
@@ -0,0 +1,22 @@
+package io.ionic.starter;
+
+import android.location.LocationManager;
+
+import com.getcapacitor.JSObject;
+import com.getcapacitor.Plugin;
+import com.getcapacitor.PluginCall;
+import com.getcapacitor.PluginMethod;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "Echo")
+public class EchoPlugin extends Plugin {
+
+  @PluginMethod()
+  public void echo(PluginCall call) {
+    String value = call.getString("value");
+
+    JSObject ret = new JSObject();
+    ret.put("value", value);
+    call.resolve(ret);
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Platform } from '@ionic/angular';
+import Echo from 'plugins/location-mocker/LocationMocker';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  constructor(
+    private platform: Platform
+  ) { }
+
+  async ngOnInit() {
+    await this.platform.ready()
+    const { value } = await Echo.echo({ value: 'Hello World!' });
+    console.log(value);
+  }
 }
